ffd370740430a4972d53108f0632ba0f7bf01382
Joyet George <joyetgeorge07@gmail.com>
2022-05-08 22:55:24 +0530

    Update share.sh

--- share.sh
+++ share.sh
@@ -1,3 +1,20 @@
-ip=`hostname -I`
+source /etc/os-release
+
+case $ID in
+    manjaro)
+        ip=`ip -json route get 8.8.8.8 | jq -r '.[].prefsrc'`
+        echo $ip
+    ;;
+    ubuntu)
+        ip=`hostname -I`
+    ;;
+    arch)
+        ip=`ip -json route get 8.8.8.8 | jq -r '.[].prefsrc'`
+        echo $ip
+    ;;
+    *)
+        ip=`hostname -I`
+    ;;
+esac
 echo you can access me @$ip:8000
 python3 -m http.server 8000
